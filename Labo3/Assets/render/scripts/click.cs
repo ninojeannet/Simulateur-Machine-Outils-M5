288ccbf77edde3ed686bc2006255623f65ef849c
Jeannet Nino <nino.jeannet@he-arc.ch>
2020-03-13 10:30:31 +0100

    texture update on click

---
+++ Labo3/Assets/render/scripts/click.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class click : MonoBehaviour
+{
+
+    public GameObject voxelSpace;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+    
+    void OnMouseDown()
+    {
+        Vector3 position =transform.position;
+        renderHandler script = voxelSpace.GetComponent<renderHandler>();
+        //Debug.Log(position);
+        script.UpdateRender(Mathf.Round(position.x),Mathf.Round(position.y),Mathf.Round(position.z));
+        Destroy(gameObject);
+    }
+}
---
+++ Labo3/Assets/render/scripts/renderHandler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class renderHandler : MonoBehaviour
+{
+
+    GameObject[] tabVoxels = new GameObject[4];
+    public Texture baseTexture;
+    public Texture workedTexture;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(transform.childCount);
+        int i=0;
+        foreach (Transform child in transform)
+        {
+            Debug.Log(child.position);
+            tabVoxels[i] = child.gameObject;
+            i++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    public void UpdateRender(float x,float y,float z)
+    {
+        Debug.Log("Bloc détruit à la position : x "+x+" y "+y+" z "+z);
+        GameObject voxel = tabVoxels[0];
+        foreach(Transform child in voxel.transform)
+        {
+            child.gameObject.GetComponent<Renderer>().material.mainTexture = workedTexture;
+            Debug.Log(child.gameObject);
+        }
+    }
+}
