61ae6abe798318dc5230c8ae612242e112995caa
Faga Davide Raffaele <davidera.faga@he-arc.ch>
2020-03-20 08:52:07 +0100

    Signed-off-by: Faga Davide Raffaele <davidera.faga@he-arc.ch>

--- Labo3/Assets/Voxel Space/Scripts/CreateVoxelSpace.cs
+++ Labo3/Assets/Voxel Space/Scripts/CreateVoxelSpace.cs
@@ -14,6 +14,10 @@ public class CreateVoxelSpace : MonoBehaviour
     public int sizeY;
     [Range(1, 100)]
     public int sizeZ;
+
+    public Texture baseTexture;
+    public Texture workedTexture;
+
     void Start()
     {
         for(int x = 0; x < sizeX; x++)
@@ -22,22 +26,67 @@ public class CreateVoxelSpace : MonoBehaviour
             {
                 for(int z = 0; z < sizeZ; z++)
                 {
+
                     Vector3 position = cube.transform.position;
                     position.x += x;
                     position.y += y;
                     position.z += z;
-
                     Quaternion rotation = cube.transform.rotation;
 
                     GameObject instance = Instantiate(cube, position, rotation);
                     instance.transform.parent = this.transform;
-                    //genre 
-                    BoxCollider bc= instance.GetComponent<BoxCollider>();
-                    bc.isTrigger = true;
-                    Transform tf = instance.GetComponent<Transform>();
-                    tf.tag = "VoxelTag";
+
+                    foreach(Transform child in instance.transform)
+                    {
+                        child.gameObject.GetComponent<Renderer>().material.mainTexture = workedTexture;
+                    }
+
+                    if(x==0)
+                    {                    
+                        GameObject plane = instance.transform.Find("Left").gameObject;
+                        plane.GetComponent<Renderer>().material.mainTexture = baseTexture;
+                    }
+                    else if(x==sizeX-1)
+                    {
+                        GameObject plane = instance.transform.Find("Right").gameObject;
+                        plane.GetComponent<Renderer>().material.mainTexture = baseTexture;
+                    }
+                    if(y==0)
+                    {
+                        GameObject plane = instance.transform.Find("Bottom").gameObject;
+                        plane.GetComponent<Renderer>().material.mainTexture = baseTexture;
+                    }
+                    if(y==sizeY-1)
+                    {
+                        GameObject plane = instance.transform.Find("Top").gameObject;
+                        plane.GetComponent<Renderer>().material.mainTexture = baseTexture;
+                    }
+                    if(z==0)
+                    {
+                        GameObject plane = instance.transform.Find("Front").gameObject;
+                        plane.GetComponent<Renderer>().material.mainTexture = baseTexture;
+                    }
+                    else if(z==sizeZ-1)
+                    {
+                        GameObject plane = instance.transform.Find("Back").gameObject;
+                        plane.GetComponent<Renderer>().material.mainTexture = baseTexture;
+                    }
+
                 }
             }
         }
     }
+
+/*
+    public void UpdateRender(float x,float y,float z)
+    {
+        Debug.Log("Bloc détruit à la position : x "+x+" y "+y+" z "+z);
+        GameObject voxel = tabVoxels[0];
+        foreach(Transform child in voxel.transform)
+        {
+            child.gameObject.GetComponent<Renderer>().material.mainTexture = workedTexture;
+            Debug.Log(child.gameObject);
+        }
+    }
+*/
 }
