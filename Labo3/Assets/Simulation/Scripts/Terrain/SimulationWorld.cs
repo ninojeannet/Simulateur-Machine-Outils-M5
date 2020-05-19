14cd396c063c402eca78cbd3d31a5c7bcb13ff01
faga <faga.davide@gmail.com>
2020-05-19 11:32:21 +0200

    change branche donc sauve

---
+++ Labo3/Assets/Simulation/Scripts/Terrain/SimulationWorld.cs
@@ -0,0 +1,93 @@
+﻿using Unity.Mathematics;
+using UnityEngine;
+using Unity.Collections;
+
+
+namespace Simulation
+{
+    /// <summary>
+    /// A SimulationWorld generated from a heightmap
+    /// </summary>
+    public class SimulationWorld : World
+    {
+        /// <summary>
+        /// The heightmap simulation generation settings
+        /// </summary>
+        [SerializeField] private SimulationSettings simulationSettings;
+
+        /// <summary>
+        /// The heightmap simulation generation settings
+        /// </summary>
+        public SimulationSettings SimulationSettings => simulationSettings;
+
+        private void Awake()
+        {
+            
+            int heightMapSize = CubeSize+2; // +2 because the heighmap needs a "border" of one pixel empty to show the walls
+            bool[,] heightmapArray = new bool[heightMapSize,heightMapSize];
+            //Generate the 2D array representing the heighmap
+            // True => material
+            // False => nothing
+            for (int x = 0; x < heightMapSize; x++)
+            {
+                for (int y = 0; y < heightMapSize; y++)
+                {
+                    if(x == 0 || y == 0 || x == heightMapSize-1 || y == heightMapSize-1)
+                        heightmapArray[x,y] = false;
+                    else
+                        heightmapArray[x,y] = true;
+                }
+            }
+            int amplitude = CubeSize;
+            simulationSettings = new SimulationSettings(heightmapArray, heightMapSize,amplitude);
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+            CreateHeightmap();
+        }
+
+        private void OnDestroy()
+        {
+            SimulationSettings.DisposeHeightmapData();
+        }
+
+        /// <summary>
+        /// Creates the heightmap and instantiates the chunks.
+        /// </summary>
+        private void CreateHeightmap()
+        {
+            int chunkCountX = Mathf.CeilToInt((float) (simulationSettings.Width - 1) / ChunkSize);
+            int chunkCountZ = Mathf.CeilToInt((float) (simulationSettings.Height - 1) / ChunkSize);
+            int chunkCountY = Mathf.CeilToInt(simulationSettings.Amplitude / ChunkSize);
+
+            for (int x = 0; x < chunkCountX; x++)
+            {
+                for (int y = 0; y < chunkCountY; y++)
+                {
+                    for (int z = 0; z < chunkCountZ; z++)
+                    {
+                        CreateChunk(new int3(x, y, z));
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Instantiates a chunk to the specified coordinate
+        /// </summary>
+        /// <param name="chunkCoordinate">The chunk's coordinate</param>
+        /// <returns>The instantiated chunk</returns>
+        private SimulationChunk CreateChunk(int3 chunkCoordinate)
+        {
+            SimulationChunk chunk = Instantiate(ChunkPrefab, (chunkCoordinate * ChunkSize).ToVectorInt(), Quaternion.identity).GetComponent<SimulationChunk>();
+            chunk.name = $"Chunk_{chunkCoordinate.x}_{chunkCoordinate.y}_{chunkCoordinate.z}";
+            chunk.World = this;
+            chunk.Initialize(ChunkSize, Isolevel, chunkCoordinate,CubeSize);
+            Chunks.Add(chunkCoordinate, chunk);
+
+            return chunk;
+        }
+    }
+}
---
+++ Labo3/Assets/Simulation/Shaders/CustomSurfaceShader.shader
@@ -0,0 +1,81 @@
+﻿Shader "Custom/CustomSurfaceShader"
+{
+    Properties
+    {
+        _Color ("Color", Color) = (1,1,1,1)
+        _CubeSize("Cube size", Int) = 128
+        _MainTex ("Main Texture", 2D) = "white" {}
+        _GlossinessFirst ("Smoothness ", Range(0,1)) = 0.5
+        _MetallicFirst ("Metallic", Range(0,1)) = 0.0
+		_SecondaryTex ("Secondary texture", 2D) = "white"{}
+        _GlossinessSecond ("Smoothness ", Range(0,1)) = 0.5
+        _MetallicSecond ("Metallic", Range(0,1)) = 0.0
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 200
+
+        CGPROGRAM
+        // Physically based Standard lighting model, and enable shadows on all light types
+        #pragma surface surf Standard fullforwardshadows
+
+        // Use shader model 3.0 target, to get nicer looking lighting
+        #pragma target 3.0
+
+        sampler2D _MainTex;
+        sampler2D _SecondaryTex;
+
+        struct Input
+        {
+            float2 uv_MainTex;
+            float2 uv2_SecondaryTex;
+            float3 worldPos;
+        };
+
+        half _GlossinessFirst;
+        half _MetallicFirst;
+        half _GlossinessSecond;
+        half _MetallicSecond;
+        fixed4 _Color;
+        int _CubeSize;
+
+        // Add instancing support for this shader. You need to check 'Enable Instancing' on materials that use the shader.
+        // See https://docs.unity3d.com/Manual/GPUInstancing.html for more information about instancing.
+        // #pragma instancing_options assumeuniformscaling
+        UNITY_INSTANCING_BUFFER_START(Props)
+            // put more per-instance properties here
+        UNITY_INSTANCING_BUFFER_END(Props)
+
+        void surf (Input IN, inout SurfaceOutputStandard o)
+        {
+            fixed4 c;
+            if (IN.worldPos[0] < 1.5 || IN.worldPos[1] < 1.5 || IN.worldPos[2] < 1.5 || IN.worldPos[0] > _CubeSize-0.5 || IN.worldPos[1] >= _CubeSize-0.5 || IN.worldPos[2] > _CubeSize-0.5)
+            {
+                c = tex2D (_MainTex, IN.uv_MainTex) * _Color;
+                o.Albedo = c.rgb;
+                // Metallic and smoothness come from slider variables
+                o.Metallic = _MetallicFirst;
+                o.Smoothness = _GlossinessFirst;
+                o.Alpha = c.a;
+            }
+            else
+            {
+                c = tex2D (_SecondaryTex, IN.uv_MainTex) * _Color;
+                o.Albedo = c.rgb;
+                // Metallic and smoothness come from slider variables
+                o.Metallic = _MetallicSecond;
+                o.Smoothness = _GlossinessSecond;
+                o.Alpha = c.a;
+            }
+            // Ici regarder en fonction de la position et afficher la texture en fonction de
+            // garder les 2 memes uvs
+            // https://docs.unity3d.com/Manual/SL-SurfaceShaders.html worldpos
+            // Albedo comes from a texture tinted by color
+            
+
+        }
+        ENDCG
+    }
+    FallBack "Diffuse"
+}
